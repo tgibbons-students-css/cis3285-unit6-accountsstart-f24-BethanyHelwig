a9c3e5f4f4a791cb3e7ef0ced542cbd8507a9e87
PhoenixVerus <73077264+PhoenixVerus@users.noreply.github.com>
2024-10-10 20:59:34 -0500

    I updated the GUI so that when an account was selected, the account balance also updated to display that account's balance.

--- cis3285_unit6_accountstart_f24/Domain/AccountBase.cs
+++ cis3285_unit6_accountstart_f24/Domain/AccountBase.cs
@@ -40,7 +40,11 @@ namespace Domain
 
         public void AddTransaction(decimal amount)
         {
-            RewardPoints += CalculateRewardPoints(amount);
+            if(amount > 0)
+            {
+                RewardPoints += CalculateRewardPoints(amount);
+            }
+
             Balance += amount;
         }
 
